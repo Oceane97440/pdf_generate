1e3b3417cbbd8ccaeaa03f2f4d032b12983df894
Oceane97440 <sautronoceane98@gmail.com>
2022-02-17 15:49:54 +0400

    1er commit test generat pdf pdf

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const puppeteer = require('puppeteer')
+
+app.use(cors());
+app.use(express.static('files'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', async function (req, res) {
+
+   
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://reporting.antennesb.fr/', {
+      waitUntil: 'networkidle2',
+    });
+  const pdf = await page.pdf({ path: 'testPDF.pdf', format: 'a4' });
+  
+    await browser.close();
+    res.send(pdf)
+});
+
+
+
+// Le serveur ecoute sur le port 3022
+app.set("port", process.env.PORT || 3001);
+
+app.listen(app.get("port"), () => {
+    console.log(`server on port ${app.get("port")}`);
+});
