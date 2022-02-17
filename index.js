c46c5e667242dfa7e96eaf6bcfdf5d3d0c8cda9e
Oceane97440 <sautronoceane98@gmail.com>
2022-02-17 16:09:28 +0400

    test

--- index.js
+++ index.js
@@ -12,16 +12,36 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', async function (req, res) {
 
-   
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto('https://reporting.antennesb.fr/', {
-      waitUntil: 'networkidle2',
-    });
-  const pdf = await page.pdf({ path: 'testPDF.pdf', format: 'a4' });
-  
-    await browser.close();
-    res.send(pdf)
+    async function generatePDF() {
+
+        //We start a new browser, without showing UI
+        const browser = await puppeteer.launch({
+            headless: true
+        });
+        const page = await browser.newPage();
+        const url = 'https://reporting.antennesb.fr/';
+
+        //We load the page, one of my blog post (networkidle0 means we're waiting for the network to stop making new calls for 500ms
+        await page.goto(url, {
+            waitUntil: 'networkidle0'
+        });
+     
+        
+
+        //Let's generate the pdf and close the browser
+        const pdf = await page.pdf({
+            path: "article.pdf",
+            format: 'A4'
+        });
+        await browser.close();
+        return res.send(pdf);
+    }
+
+    generatePDF();
+
+
+
+
 });
 
 
