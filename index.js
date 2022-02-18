77600c16d6d65ffb212465116fc881490707fc70
Oceane97440 <sautronoceane98@gmail.com>
2022-02-18 10:45:21 +0400

    test export proxy

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 const puppeteer = require('puppeteer')
-const puppeteer_extra = require('puppeteer-extra')
+//const puppeteer_extra = require('puppeteer-extra')
+//const puppeteer_proxy = require('puppeteer-proxy')
+//const proxyRequest = require('puppeteer-proxy')
+//const puppeteer = require('zyte-smartproxy-puppeteer');
 
 app.use(cors());
 app.use(express.static('files'));
@@ -12,7 +15,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', async function (req, res) {
-    
+
     try {
         async function generatePDF() {
 
@@ -23,20 +26,20 @@ app.get('/', async function (req, res) {
                     "--disable-setuid-sandbox",
                     "--disable-dev-shm-usage",
                     "--single-process"
-        
+
                 ],
             });
             const page = await browser.newPage();
             const url = 'https://reporting.antennesb.fr/';
             res.setHeader("Content-Type", "application/pdf");
-    
+
             //We load the page, one of my blog post (networkidle0 means we're waiting for the network to stop making new calls for 500ms
             await page.goto(url, {
                 waitUntil: 'networkidle0'
             });
-    
-    
-    
+
+
+
             //Let's generate the pdf and close the browser
             const pdf = await page.pdf({
                 path: "article.pdf",
@@ -45,13 +48,13 @@ app.get('/', async function (req, res) {
             await browser.close();
             return res.send(pdf);
         }
-    
+
         generatePDF();
     } catch (error) {
-        res.send(`❌ Error: ${error.message}`);    
+        res.send(`❌ Error: ${error.message}`);
     }
 
-  
+
 
 
 
@@ -86,6 +89,57 @@ app.get('/export', async function (req, res) {
         await browser.close()
         res.send(pdf)
     })
+
+
+
+
+});
+
+
+app.get('/proxy', async function (req, res) {
+
+    try {
+
+      
+      
+
+        (async () => {
+            const browser = await puppeteer.launch({
+                ignoreHTTPSErrors: true,
+                headless: false,
+                args: [
+                    '--proxy-server=localhost:3OO1'
+                ]
+            });
+            const page = await browser.newPage({
+                ignoreHTTPSErrors: true
+            });
+
+            console.log('Opening page ...');
+            try {
+
+                await page.goto('https://toscrape.com/', {timeout: 180000});
+            } catch (err) {
+                console.log(err);
+            }
+
+            console.log('Taking a screenshot ...');
+            /*const pdf = await page.pdf({
+                path: "article.pdf",
+                format: 'A4'
+            });*/
+           const pdf = await page.screenshot({path: 'screenshot.png'});
+
+            await browser.close();
+            return res.send(pdf);
+
+        })();
+
+    } catch (error) {
+        res.send(`❌ Error: ${error.message}`);
+    }
+
+
 
 
 
